1f01dfc06705ec21eed8d58ba3cbf75eb61344ef
AI-Jiny <key140@naver.com>
2024-02-19 20:46:00 +0900

    add row&column widget to body

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -10,17 +10,38 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Hello world!',
-            style: TextStyle(
-              color: Colors.red,
-            ),
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                    Text(
+                      'Hey, Yj ',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                      ),
+                    ),
+                  ])
+                ],
+              )
+            ],
+          )),
+    ));
   }
 }
